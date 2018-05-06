5ee86fb3fbb0e94b716895fdaa09b0d0697695e2
S. Simeonov <svilborg@gmail.com>
2018-05-06 15:09:14 +0300

    Organize bins

--- bin/bbclean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bleachbit -p libreoffice.* java.* system.trash chromium.cache chromium.cookies google_chrome.cache google_chrome.cookies system.tmp firefox.cache firefox.download_history firefox.cookies gnome.* opera.* epiphany.* thumbnails.cache kde.* flash.*  x11.debug_logs
--- bin/reload_bashrc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-. ~/.bashrc; 
-echo "Reloaded .bashrc"
